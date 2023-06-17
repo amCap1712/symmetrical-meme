270913df6b8cad6962f1a7cf98fbaca27c2c340d
Kartik Ohri <kartikohri13@gmail.com>
2023-06-17 16:34:59 +0530

    Q1: Improve address validation and add test cases

--- Q1/pincode.py
+++ Q1/pincode.py
@@ -1,3 +1,5 @@
+import sys
+
 import requests
 
 from models import PostOfficeT, ResponseT
@@ -18,12 +20,20 @@ def get_post_offices(pincode: str) -> list[PostOfficeT] | None:
     return data["PostOffice"]
 
 
-def get_pincode(address: str):
-    """ Extract and return the pincode from the given address.
+def get_pincode(address: str) -> str | None:
+    """ Extract and return the pincode from the given address. """
+    # removes full-stop and commas from the address to improve chances of extracting the pincode
+    address = address.replace(".", "")
+    address = address.replace(",", "")
 
-        Assumes the last number (6 digits) of the address are always the pincode.
-    """
-    return address[-6:]
+    # iterate from last to front and whenever a block of 6 digits is encountered, extract it as the pincode
+    words = address.split()
+    words.reverse()
+    for word in words:
+        if word.isdigit() and len(word) == 6:
+            return word
+
+    return None
 
 
 def verify_address(address) -> bool:
@@ -32,14 +42,40 @@ def verify_address(address) -> bool:
     :param address: the address to check
     :return: the address is correct or not
     """
+    address = address.lower()
+
     pincode = get_pincode(address)
+    if pincode is None:
+        return False
+
     post_offices = get_post_offices(pincode)
     for post_office in post_offices:
-        if post_office["Name"] in address:
+        if post_office["Name"].lower() in address:
             return True
+
     return False
 
 
+def run_tests() -> int:
+    testcases = [
+        ("2nd Phase, 374/B, 80 Feet Rd, Bank Colony, Banashankari 3rd Stage, Srinivasa Nagar, Bengaluru, Karnataka 560050", True),
+        ("374/B, 80 Feet Rd, State Bank Colony, Banashankari 3rd Stage, Srinivasa Nagar, Bangalore. 560050", True),
+        ("374/B, 80 Feet Rd, State Bank Colony, Banashankari 3rd Stage, Srinivasa Nagar, Bangalore. 560050.", True),
+        ("374/B, 80 Feet Rd, State Bank Colony, Banashankari 3rd Stage, Srinivasa Nagar, Bangalore. 560050. Karnataka", True),
+        ("2nd Phase, 374/B, 80 Feet Rd, Mysore Bank Colony, Banashankari 3rd Stage, Srinivasa Nagar, Bengaluru, Karnataka 560095", False),
+        ("Colony, Bengaluru, Karnataka 560050", False),
+    ]
+
+    failures = 0
+    for address, expected in testcases:
+        received = verify_address(address)
+        if expected != received:
+            print(f"Test Failed. Address: '{address}'. Expected: {expected}. Received: {received}.", file=sys.stderr)
+            failures += 1
+    return failures
+
+
 if __name__ == '__main__':
-    addr = input("Enter address: ")
-    print(verify_address(addr))
+    count = run_tests()
+    if count:
+        sys.exit(f"{count} test failure(s).")
